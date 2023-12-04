83cfc6339ca89928eb1e8b689821df6b5a5ad947
TomL <addr.toml@gmail.com>
2023-12-04 23:49:05 +0200

    d3p1 better

--- src/day03/part1.rs
+++ src/day03/part1.rs
@@ -1,63 +1,11 @@
-const INPUT: &'static str = include_str!("../../data/day03/input.txt");
+use crate::schematic::Schematic;
 
 mod schematic;
 
+const INPUT: &'static str = include_str!("../../data/day03/input.txt");
+
 fn main() {
-    let sum: usize = INPUT
-        .lines()
-        .map(|line| {
-            line.chars()
-                .enumerate()
-                .filter(|&(_, c)| c.is_digit(10))
-                .fold(Vec::<(usize, String)>::new(), |mut v, (column, digit)| {
-                    // if current digit is a part of the current number, add to it
-                    // else start a new number
-                    if let Some((_, number)) = v
-                        .last_mut()
-                        .filter(|(number_column, number)| *number_column + number.len() == column)
-                    {
-                        number.push(digit);
-                    } else {
-                        v.push((column, digit.to_string()));
-                    }
-                    v
-                })
-                .into_iter()
-        })
-        .enumerate()
-        .map(|(line, column_and_number)| {
-            column_and_number.map(move |(column, number)| (line, column, number))
-        })
-        .flatten()
-        .filter(|(number_line, number_start_column, number)| {
-            // block range
-            let start_line = number_line.checked_sub(1).unwrap_or(0);
-            let end_line = number_line + 1;
-            let start_col = number_start_column.checked_sub(1).unwrap_or(0);
-            let end_col = number_start_column + number.len();
-
-            // get number of special chars in the block
-            let special_chars_in_block = INPUT
-                .lines()
-                .into_iter()
-                .skip(start_line)
-                .take(end_line - start_line + 1)
-                .map(|line| {
-                    line.chars()
-                        .into_iter()
-                        .skip(start_col)
-                        .take(end_col - start_col + 1)
-                })
-                .flatten()
-                .filter(|c| !c.is_digit(10) && *c != '.')
-                .count();
-
-            special_chars_in_block > 0
-        })
-        .filter_map(|(_, _, number)| number.parse::<usize>().ok())
-        .sum();
-    println!("{:?}", sum);
-    // find numbers, for each number find start and end column
-    // from that gather the block
-    // search block for symbols
+    let schematic = Schematic::new(INPUT);
+    let result: usize = schematic.get_part_numbers().sum();
+    println!("{:?}", result);
 }
--- src/day03/schematic.rs
+++ src/day03/schematic.rs
@@ -7,12 +7,75 @@ pub struct Schematic {
 impl Schematic {
     pub fn new(input: &str) -> Self {
         Self {
-            matrix: input.lines().map(|line| line.chars().collect()).collect(),
+            matrix: input.lines().map(|s| s.chars().collect_vec()).collect_vec(),
         }
     }
 
-    pub fn get_part_numbers(&self) -> impl Iterator<Item = usize> {
-        
-        std::iter::empty()
+    pub fn get_part_numbers(&self) -> impl Iterator<Item = usize> + '_ {
+        self.get_numbers_in_matrix()
+            .filter_map(|(line, col)| {
+                self.get_number_at_index(line, col)
+                    .map(|number_str| (line, col, number_str))
+            })
+            .filter(|(line, col, number_str)| {
+                let start_line = line.checked_sub(1).unwrap_or(0);
+                let end_line = line + 1;
+                let start_col = col.checked_sub(1).unwrap_or(0);
+                let end_col = col + number_str.len();
+
+                let special_chars_in_block = self
+                    .get_chars_in_block(start_line, end_line, start_col, end_col)
+                    .filter(|c| !c.is_digit(10) && *c != '.')
+                    .count();
+                special_chars_in_block > 0
+            })
+            .filter_map(|(_, _, number_str)| number_str.parse::<usize>().ok())
+    }
+
+    fn get_numbers_in_matrix(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        self.matrix
+            .iter()
+            .map(|line| {
+                line.iter().enumerate().filter_map(|(column, char)| {
+                    let previous_char = column
+                        .checked_sub(1)
+                        .and_then(|prev_col| line.get(prev_col));
+
+                    if !char.is_digit(10) || previous_char.is_some_and(|c| c.is_digit(10)) {
+                        None
+                    } else {
+                        Some(column)
+                    }
+                })
+            })
+            .enumerate()
+            .map(|(line, columns)| columns.map(move |column| (line, column)))
+            .flatten()
+    }
+
+    fn get_number_at_index(&self, line: usize, col: usize) -> Option<String> {
+        self.matrix.get(line)?.get(col..).map(|slice| {
+            slice
+                .into_iter()
+                .take_while(|c| c.is_digit(10))
+                .cloned()
+                .collect::<String>()
+        })
+    }
+
+    fn get_chars_in_block(
+        &self,
+        start_line: usize,
+        end_line: usize,
+        start_col: usize,
+        end_col: usize,
+    ) -> impl Iterator<Item = char> + '_ {
+        self.matrix
+            .iter()
+            .skip(start_line)
+            .take(end_line - start_line + 1)
+            .map(move |line| line.iter().skip(start_col).take(end_col - start_col + 1))
+            .flatten()
+            .cloned()
     }
 }
