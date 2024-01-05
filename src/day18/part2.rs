f6e524f8673c0ef12dd9ddd66ec286c3fccb95fd
TomDev5 <addr.toml@gmail.com>
2024-01-05 19:07:44 +0200

    d18p2 duplicated

---
+++ src/day18/part2.rs
@@ -0,0 +1,83 @@
+const INPUT: &str = include_str!("../../data/day18/input.txt");
+
+fn main() {
+    let corners = INPUT
+        .lines()
+        .filter_map(|line| line.split_ascii_whitespace().skip(2).next())
+        .map(|line| &line[2..line.len() - 1])
+        .filter_map(|line| {
+            let direction = match line.chars().nth(5)? {
+                '0' => Direction::R,
+                '1' => Direction::D,
+                '2' => Direction::L,
+                '3' => Direction::U,
+                _ => {
+                    return None;
+                }
+            };
+            let distance = isize::from_str_radix(&line[0..5], 16).ok()?;
+            Some((direction, distance))
+        })
+        .fold(vec![(0, 0)], |corners, current_move| {
+            let previous_corner = *corners.last().unwrap();
+            let mut corners = corners.clone();
+            corners.push(match current_move.0 {
+                Direction::U => (previous_corner.0 - current_move.1, previous_corner.1),
+                Direction::D => (previous_corner.0 + current_move.1, previous_corner.1),
+                Direction::L => (previous_corner.0, previous_corner.1 - current_move.1),
+                Direction::R => (previous_corner.0, previous_corner.1 + current_move.1),
+            });
+            corners
+        });
+
+    println!("result: {:?}", picks(&corners));
+}
+
+fn picks(corners: &Vec<(isize, isize)>) -> isize {
+    let outline_length = corners
+        .iter()
+        .fold((0, (0, 0)), |(length, previous), point| {
+            (
+                length
+                    + isize::abs_diff(previous.0, point.0)
+                    + isize::abs_diff(previous.1, point.1),
+                *point,
+            )
+        })
+        .0 as isize;
+    shoelace(corners) - (outline_length / 2) + 1 + outline_length
+}
+
+fn shoelace(corners: &Vec<(isize, isize)>) -> isize {
+    let mut area = 0isize;
+
+    for i in 0..corners.len() {
+        let j = (i + 1) % corners.len();
+        area += corners[i].0 * corners[j].1;
+        area -= corners[j].0 * corners[i].1;
+    }
+
+    area.abs() / 2
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Direction {
+    U,
+    D,
+    L,
+    R,
+}
+
+impl TryFrom<char> for Direction {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'U' => Ok(Self::U),
+            'D' => Ok(Self::D),
+            'L' => Ok(Self::L),
+            'R' => Ok(Self::R),
+            _ => Err(()),
+        }
+    }
+}
