36c1aa9e11c17a88ff19ff1758dd5bb95458f5a8
TomDev5 <addr.toml@gmail.com>
2024-01-13 17:31:08 +0200

    simplified

--- src/day20/part2.rs
+++ src/day20/part2.rs
@@ -1,8 +1,5 @@
-use std::collections::HashMap;
-
-use itertools::Itertools;
-
 use crate::circuit::Circuit;
+use std::collections::HashMap;
 
 mod circuit;
 mod communication_module;
@@ -13,40 +10,41 @@ fn main() {
     let mut circuit = Circuit::new(INPUT).expect("Invalid input");
 
     let mut semifinal_cycles: HashMap<String, (usize, usize)> = HashMap::from_iter([
-        (String::from("sg"), (0, 0)),
-        (String::from("lm"), (0, 0)),
-        (String::from("dh"), (0, 0)),
-        (String::from("db"), (0, 0)),
+        ("sg".to_string(), (0, 0)),
+        ("lm".to_string(), (0, 0)),
+        ("dh".to_string(), (0, 0)),
+        ("db".to_string(), (0, 0)),
     ]);
+
     let mut i = 0;
-    loop {
+    while semifinal_cycles.values().any(|(_, b)| *b == 0) {
         i += 1;
         let pulse_counters = circuit.press_button();
-        for (name, (a, b)) in &mut semifinal_cycles {
-            let Some((_, current_high)) = pulse_counters.get(name) else {
-                continue;
-            };
-            if *current_high == 0 {
-                continue;
-            }
 
-            if *a == 0 {
-                *a = i;
-            } else if *b == 0 {
-                *b = i;
-            }
-        }
+        for (semifinal_module_name, (first_hit_iteration, second_hit_iteration)) in
+            &mut semifinal_cycles
+        {
+            if let Some((_, current_high)) = pulse_counters.get(semifinal_module_name) {
+                if *current_high == 0 {
+                    continue;
+                }
 
-        if semifinal_cycles.values().all(|(_, b)| *b != 0) {
-            break;
+                if *first_hit_iteration == 0 {
+                    *first_hit_iteration = i;
+                } else if *second_hit_iteration == 0 {
+                    *second_hit_iteration = i;
+                }
+            }
         }
     }
 
-    println!("semifinal cycles: {:?}", semifinal_cycles);
-    println!(
-        "semifinal cycles: {:?}",
-        semifinal_cycles.values().map(|(a, b)| b - a).fold(1, lcm)
-    );
+    let result = semifinal_cycles
+        .values()
+        .cloned()
+        .map(|(a, b)| usize::abs_diff(a, b))
+        .fold(1, lcm);
+
+    println!("result: {:?}", result);
 }
 
 fn lcm(a: usize, b: usize) -> usize {
