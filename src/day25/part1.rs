47d48b71bee66afc46b7e3cedeb92c6889c07596
TomDev5 <addr.toml@gmail.com>
2024-02-07 22:48:35 +0200

    d25p1 pretty

--- src/day25/part1.rs
+++ src/day25/part1.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-use itertools::Itertools;
+use itertools::{iproduct, Itertools};
 
 const INPUT: &str = include_str!("../../data/day25/input.txt");
 
@@ -8,70 +8,56 @@ fn main() {
     let mut connections = INPUT
         .lines()
         .filter_map(|line| line.split(':').collect_tuple())
-        .map(|(from, to)| {
+        .flat_map(|(from, to)| {
             to.split_whitespace()
-                .map(move |to| vec![(from, to), (to, from)])
+                .flat_map(move |to| vec![(from, to), (to, from)])
         })
-        .flatten()
-        .flatten()
         .sorted_by_key(|(from, _)| *from)
         .group_by(|(from, _)| *from)
         .into_iter()
-        .map(|(from, to)| (from, to.map(|(_, to)| to).collect_vec()))
+        .map(|(from, to)| (from, to.map(|(_, to)| to).collect::<HashSet<_>>()))
         .collect::<HashMap<_, _>>();
 
     let vertices = connections.keys().cloned().collect::<Vec<_>>();
     let costs = connections
         .iter()
-        .map(|(from, to)| to.into_iter().map(move |to| ((*from, *to), 1isize)))
-        .flatten()
+        .flat_map(|(from, to)| to.iter().map(move |to| ((*from, *to), 1isize)))
         .collect::<Vec<_>>();
 
-    let mut minimal_cuts = Vec::new();
-    let mut source = "";
-    let mut sink = "";
-    for sinkstart in connections.keys().combinations(2) {
-        let current_source = sinkstart[0];
-        let curret_sink = sinkstart[1];
-
-        let max_flow = pathfinding::directed::edmonds_karp::edmonds_karp_dense(
-            &vertices,
-            current_source,
-            curret_sink,
-            costs.clone(),
-        );
-
-        if max_flow.1 == 3 {
-            minimal_cuts = max_flow.2.into_iter().map(|(point, _)| point).collect_vec();
-            source = current_source;
-            sink = curret_sink;
-            break;
-        }
-    }
+    let (source, sink, minimal_cuts) = iproduct!(connections.keys(), connections.keys())
+        .map(|(source, sink)| {
+            let max_flow = pathfinding::directed::edmonds_karp::edmonds_karp_dense(
+                &vertices,
+                source,
+                sink,
+                costs.clone(),
+            );
+            (
+                *source,
+                *sink,
+                max_flow.2.into_iter().map(|(point, _)| point).collect_vec(),
+            )
+        })
+        .find(|(_, _, minimal_cut)| minimal_cut.len() == 3)
+        .expect("No minimal cut sized 3 found");
 
     for minimal_cut in minimal_cuts {
-        let Some(tos) = connections.get_mut(&minimal_cut.0) else {
-            continue;
-        };
-        let Some(position) = tos.iter().position(|to| to == &minimal_cut.1) else {
-            continue;
-        };
-        tos.remove(position);
-        let Some(tos) = connections.get_mut(&minimal_cut.1) else {
-            continue;
-        };
-        let Some(position) = tos.iter().position(|to| to == &minimal_cut.0) else {
-            continue;
-        };
-        tos.remove(position);
+        connections
+            .get_mut(&minimal_cut.0)
+            .expect("Inavlid node")
+            .remove(minimal_cut.1);
+        connections
+            .get_mut(&minimal_cut.1)
+            .expect("Inavlid node")
+            .remove(minimal_cut.0);
     }
 
     let source_group = pathfinding::directed::bfs::bfs_reach(source, |point| {
-        connections.get(point).unwrap().clone()
+        connections.get(point).expect("Inavlid point").clone()
     });
 
     let sink_group = pathfinding::directed::bfs::bfs_reach(sink, |point| {
-        connections.get(point).unwrap().clone()
+        connections.get(point).expect("Inavlid point").clone()
     });
 
     println!("{}", source_group.count() * sink_group.count());
